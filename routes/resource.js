b9246acb11dcfcde22c12261a93102fd776374b9
Dan Ross <ross9885@gmail.com>
2013-02-11 17:14:38 -0800

    Created /:resource route.

---
+++ routes/resource.js
@@ -0,0 +1,43 @@
+
+/*
+ * GET a page for a resource or GET the index.
+ */
+
+var fs = require('fs');
+var md = require('node-markdown').Markdown;
+
+var defaults = {
+	title: 'codes and internets and the goo between them',
+	resourceName: 'index',
+	template: 'index',
+	filename: 'index.md'
+};
+
+exports.resource = function(request, response){
+	// Use the resource named in the GET request if specified, otherwise render the home page according to defaults.
+	var resourceName = request.params.resource? request.params.resource: defaults.resourceName;
+
+	// get the resource's meta info
+	var meta;
+	try {
+		meta = JSON.parse(fs.readFileSync(resourceName + '.json'));
+	} catch(error) {
+		console.log(error);
+		response.send(404);
+		return;
+	}
+
+	var filename = meta.filename? meta.filename: defaults.filename;
+	
+	// read, and if necessary parse, the resource's content
+	// throws ENOENT if the file doesn't exist
+	var content = fs.readFileSync('content/' + filename);
+	if(filename.match(/.md$/i)) content = md(content);
+
+	// render the resource using it's template and the other metadata
+	var template = meta.template? meta.template: defaults.template;
+	response.render(template, {
+		title: (meta.title? meta.title: defaults.title) + ' - abstractGOO',
+		content: content
+	});
+};
